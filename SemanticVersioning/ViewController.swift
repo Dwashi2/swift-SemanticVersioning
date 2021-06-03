69ac033e627989fca2afe9f884c3000113682e29
Daniel Washington <danielvertigo@hotmail.com>
2021-06-03 20:20:45 -0300

    Initial Commit

---
+++ SemanticVersioning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SemanticVersioning
+//
+//  Created by Daniel Washington Ignacio on 03/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
